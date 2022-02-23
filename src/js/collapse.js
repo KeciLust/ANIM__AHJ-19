3335808fdffe71b92faf5ffc0ca9524b90695b0a
Denis Nikitin <dinya.nikitin@gmail.com>
2022-02-23 20:29:26 +0300

    change

--- src/js/collapse.js
+++ src/js/collapse.js
@@ -1,5 +1,26 @@
 export default class Collapse {
-    constructor() {
+  constructor(element) {
+    this.element = element;
+  }
 
-    }
+  addContainer() {
+    const containerWidget = document.querySelector('.collapse');
+    const button = document.createElement('button');
+    button.classList.add('button');
+    button.textContent = 'Collapse';
+    this.element.classList.add('element');
+    containerWidget.append(button);
+    containerWidget.append(this.element);
+  }
+
+  listner() {
+    const button = document.querySelector('.button');
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+    });
+  }
+
+  anime() {
+
+  }
 }
